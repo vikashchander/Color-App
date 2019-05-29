5b618a52d16c9860de6d36efd5857fd317879f98
vikash chander <vikashchander78384@gmail.com>
2019-05-29 11:01:23 +0530

    add styles ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,11 +4,27 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends  Component{
+      constructor(props){
+           super(props);
+           this.state= {copied:false};
+           this.changeState = this.changeState.bind(this);
+      }
+    changeState(){
+         this.setState({copied:true}, ()=>
+             setTimeout(  ()=> this.setState({copied:false}),2000)
+          ) };
+
      render() {
          const {name,background} =this.props;
+         const {copied} = this.state;
          return (
-            <CopyToClipboard text={background} >
-            <div style={{background}} className='ColorBox'>
+            <CopyToClipboard text={background} onCopy={this.changeState} >
+            <div style={{background}} className='ColorBox' >
+            <div style={{background}} className={` copy-overlay ${copied && 'show' }`}  />
+            <div class={` copy-msg ${copied && 'show' }`} >
+            <h2>Copied</h2>
+            <p>{background}</p>
+            </div>
              <div  className='copy-content'>
                   <div  className='box-content'>
                   <span>{name}</span>          
