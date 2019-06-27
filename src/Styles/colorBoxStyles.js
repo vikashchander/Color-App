4b087f625f83e66930a8178a1ff9ec53892b751b
vikash chander <vikashchander78384@gmail.com>
2019-06-27 22:45:41 +0530

    add mediaqueries to colorBoxStyles

--- src/Styles/colorBoxStyles.js
+++ src/Styles/colorBoxStyles.js
@@ -1,4 +1,6 @@
 import chroma from 'chroma-js';
+import sizes from './sizes';
+
 export default {
     ColorBox: {
       width: "20%",
@@ -10,6 +12,18 @@ export default {
       marginBottom: "-4.5px",
       "&:hover button": {
         opacity: 1
+      },
+      [sizes.down('lg')]:{
+        width:'25%',
+        height: props => (props.showFullPalette ? "20%" : "50%"),
+      },
+      [sizes.down('md')]:{
+        width:'50%',
+        height: props => (props.showFullPalette ? "10%" : "50%"),
+      },
+      [sizes.down('xs')]:{
+        width:'100%',
+        height: props => (props.showFullPalette ? "5%" : "50%"),
       }
     },
     colorName:{
